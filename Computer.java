ac61ef6a9987151b348fafac36c6bbc387afd5c8
Madie <madina9696@gmail.com>
2014-06-03 21:10:55 -0400

    Card
    change in constuctor

--- Computer.java
+++ Computer.java
@@ -7,16 +7,18 @@ import java.io.*;
 public class Computer {
 
     private Hand hand;
- 
 
     //CONSTRUCTOR: must set up hand
-    public Computer(){
-	hand = new Hand([INSERT ARRAYLIST HERE]);
+    public Computer(ArrayList<Cards> cards){
+        cards.add(
+        hand = new Hand(cards);
     }
 
+    
+                  
     //public accessor to hand
     public Hand getHand() {
-	return hand;
+            return hand;
     }
 }
 
