3eb97a82775633acca8017ce08f8ca5226fadac1
angela97lin <angela97lin@gmail.com>
2014-06-06 14:16:11 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -22,55 +22,20 @@ public class Driver extends JFrame{
 	//add(p);
 	setVisible(true);
 	getContentPane().setBackground(Color.GREEN);
-    }
-
-
-    public static void main(String[] args){
-	Driver d = new Driver();
-    }
-
-
-
-
-}
-=======
-import javax.swing.*;
-import java.awt.*;
-import javax.imageio.*;
-import java.util.*;
-import java.io.*;
-
-public class Driver extends JFrame{
-
-    public static Dimension size = new Dimension(1000,730);
-    public static String name = "BS";
-
-    public Driver(){
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setSize(size);
-	setTitle(name);
-	setLocationRelativeTo(null);//centers screen
-	setResizable(false);
-	setLayout(new GridLayout(1,1,0,0));
-   	//Panel p = new Panel(this);
-	//add(p);
-	setVisible(true);
-	Music m = new Music();
+		Music m = new Music();
 	m.play();
     }
 
 
-
-
-
     public static void main(String[] args){
 	Driver d = new Driver();
-
     }
+
+
+
 
 
 
 
 }
 
->>>>>>> FETCH_HEAD
